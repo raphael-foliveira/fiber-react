85ef1bc3679458acbce8cfe693b328a2d1ba9470
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-10-26 09:14:50 -0300

    feat: create dtos for users and todos

---
+++ backend/internal/dto/users.go
@@ -0,0 +1,21 @@
+package dto
+
+type CreateUser struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type UpdateUser struct {
+	Password string `json:"password"`
+}
+
+type User struct {
+	ID    int
+	Email string
+}
+
+type UserWithTodos struct {
+	ID    int
+	Email string
+	Todos []Todo
+}
