4d91348f81bbf6ae2cf2a276ef78ed0b370b1916
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-10-30 23:09:31 -0300

    feat: move logic to handle authenticated user to its own hook

---
+++ frontend/src/hooks/useAuthenticatedUser.ts
@@ -0,0 +1,41 @@
+import { useContext, useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../contexts/authContext';
+import { authService } from '../service/authService';
+import { AuthData } from '../types/auth';
+
+export function useAuthenticatedUser() {
+  const { authData, setAuthData, clearAuthData } = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const handleAuthenticatedUser = async ({
+      refreshToken,
+      user,
+      accessToken,
+    }: AuthData) => {
+      try {
+        if (refreshToken && user.id) {
+          const { accessToken } = await authService.refreshToken({
+            refreshToken,
+            userId: user.id,
+          });
+          setAuthData({
+            ...authData,
+            accessToken,
+          });
+          navigate('/todos');
+        }
+      } catch {
+        authService.logout({ accessToken });
+        clearAuthData();
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    handleAuthenticatedUser(authData);
+  }, []);
+
+  return isLoading;
+}
--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -1,42 +1,13 @@
-import { Suspense, lazy, useContext, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
 import Loading from '../../components/Loading/Loading';
-import { AuthContext } from '../../contexts/authContext';
+import { useAuthenticatedUser } from '../../hooks/useAuthenticatedUser';
 import { useDocumentTitle } from '../../hooks/useDocumentTitle';
-import { authService } from '../../service/authService';
 
 const LoginForm = lazy(() => import('../../components/Forms/Login/Login'));
 
 export function Login() {
   useDocumentTitle('Login');
-  const { authData, setAuthData, clearAuthData } = useContext(AuthContext);
-  const [isLoading, setIsLoading] = useState(true);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const handleAuthenticatedUser = async () => {
-      const { refreshToken, user, accessToken } = authData;
-      try {
-        if (refreshToken && user.id) {
-          const { accessToken } = await authService.refreshToken({
-            refreshToken,
-            userId: user.id,
-          });
-          setAuthData({
-            ...authData,
-            accessToken,
-          });
-          navigate('/todos');
-        }
-      } catch {
-        authService.logout({ accessToken });
-        clearAuthData();
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    handleAuthenticatedUser();
-  }, []);
+  const isLoading = useAuthenticatedUser();
 
   return (
     <>
